f71d15c5f899eaa85e2a7dab734ab117e53879ea
Asip Hamdi <asiphamdi@gmail.com>
2023-03-31 14:53:51 +0700

    update

--- resources/views/pages/rtrw/form_ketua_rt.blade.php
+++ resources/views/pages/rtrw/form_ketua_rt.blade.php
@@ -64,8 +64,7 @@
                                     {{ date_diff(new \DateTime($i->awal_menjabat), new \DateTime($i->akhir_menjabat))->format("%y Tahun, %m Bulan, %d Hari"); }}
                                 </td>
                                 <td class="text-center">
-                                    {{ $i->user }}
-                                    {{-- {{ $i->user ? 'Aktif' : 'Tidak Aktif' }} --}}
+                                    {{ $i->user->s_aktif == 1 ? 'Aktif' : 'Tidak Aktif' }}
                                 </td>
                                 <td class="text-center">
                                     @if ($i->status == 1)
